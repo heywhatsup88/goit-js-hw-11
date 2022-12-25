7b3265bbddf4030d41160d39b8c9fd052974bc9c
heywhatsup88 <heywhatsup@ukr.net>
2022-12-25 05:08:27 +0300

    .

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,13 +1,3 @@
-// import axios from 'axios';
-
-// export default async function fetch(value, page) {
-//   const url = 'https://pixabay.com/api/';
-//   const key = '32252301-c8c1c4efb1ef52864619e43eb';
-//   const filter = `key=${key}&q=${value}&image_type=photo&min_width=800&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
-
-//   return await axios.get(`${url}?${filter}`).then(response => response.data);
-// }
-
 import axios from 'axios';
 
 export default async function fetch(value, page) {
