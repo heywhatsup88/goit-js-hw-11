878f7b3ed81b28e56cd314f45951cebeeea74c5e
heywhatsup88 <heywhatsup@ukr.net>
2022-12-25 15:44:59 +0300

    .

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -1,7 +1,7 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-let lightbox = new SimpleLightbox('.photo-card a', {
+const lightbox = new SimpleLightbox('.photo-card a', {
   captions: true,
   captionsData: 'alt',
   captionDelay: 250,
